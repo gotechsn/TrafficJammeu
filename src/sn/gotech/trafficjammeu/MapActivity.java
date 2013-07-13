d5a224182727998b9d7f9ce939a480becf309583
Souleymane Thiongane <mansoulx@hotmail.com>
2013-07-13 03:03:40 +0100

    Revert "Adding marqued routes to json fil"
    This reverts commit 55ec0c1947f773fb7e70a15a7a00fc9b30920abe.

--- src/sn/gotech/trafficjammeu/MapActivity.java
+++ src/sn/gotech/trafficjammeu/MapActivity.java
@@ -704,8 +704,8 @@ public class MapActivity extends Activity implements OnMapLongClickListener, Loc
 		values.add(new BasicNameValuePair("lng1st", lng1st));
 		values.add(new BasicNameValuePair("lat2nd", lat2nd));
 		values.add(new BasicNameValuePair("lng2nd", lng2nd));
-		values.add(new BasicNameValuePair("user", user));
 		values.add(new BasicNameValuePair("desc", desc));
+		values.add(new BasicNameValuePair("user", user));
 		HttpClient client = new DefaultHttpClient();
 		HttpPost post = new HttpPost(URL_TO_PHP_FILE);
 		try {
