e21639d67d1506cf3595b0c77ff7971e5584e0ce
Souleymane Thiongane <mansoulx@hotmail.com>
2013-08-15 13:56:21 +0000

    design improved

---
+++ src/sn/gotech/trafficjammeu/TransparentSupportMapFragment.java
@@ -0,0 +1,27 @@
+package sn.gotech.trafficjammeu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class TransparentSupportMapFragment extends SupportMapFragment {
+    public TransparentSupportMapFragment() {
+
+        super();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup view, Bundle savedInstance) {
+
+        View layout = super.onCreateView(inflater, view, savedInstance);
+        FrameLayout frameLayout = new FrameLayout(getActivity());
+        frameLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        ((ViewGroup) layout).addView(frameLayout, new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        return layout;
+    }
+}
